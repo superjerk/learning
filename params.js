4b9ec0d92b54013f7073abdd977f50a7a064810b
superjerk <tungsten33@gmail.com>
2017-01-10 15:31:21 -0600

    Update params.js

--- params.js
+++ params.js
@@ -3,8 +3,5 @@ var EnergyGen = ['Combustion','Fission','Fusion','Solar'],
     Hull = ['Ferric','Aluminum','Ceramics','Plastics'],
     Shield = ['EM','Charged','Phasing','Deflecting'],
     MassWep = ['Bulletstorm','Railgun','Missile','Rocket'],	
-    Energy Weapon	Plasma Weapon
-Combustion	Batteries	Ferric	EM	Bulletstorm	Laser	Beam
-Fission	Hydrogen	Aluminum	Charged Parti	Railgun	Blaster	Blade
-Fusion	Capacitors	Ceramics	Phasing	Missile	Photon Torpedo	Blob
-Solar	Flywheel	Plastics	Deflecting	Rocket	Ion Cannon	Rope/Hose
+    EnergyWep = ['Laser','Blaster','Torpedo','Ion Cannon'],	
+    PlasmaWep = ['Beam','Blade','Blob','Rope']
