7c0bca90cdce81289141c12fc974e8c5ce9c2edb
superjerk <tungsten33@gmail.com>
2017-01-10 11:09:12 -0600

    Update learning.js

--- learning.js
+++ learning.js
@@ -1,10 +1,13 @@
-alert("learned twice");
-var canvas = document.getElementById("starfield"),
-context = canvas.getContext("2d"),
-stars = 200;
-for (var i = 0; i < stars; i++) {
-    x = Math.random() * canvas.offsetWidth;
-    y = Math.random() * canvas.offsetHeight;
-   context.fillStyle = "white";
-    context.fillRect(x,y,1,1);
+function codeAddress() {
+    alert("learned twice");
+    var canvas = document.getElementById("starfield"),
+    context = canvas.getContext("2d"),
+    stars = 200;
+    for (var i = 0; i < stars; i++) {
+        x = Math.random() * canvas.offsetWidth;
+        y = Math.random() * canvas.offsetHeight;
+        context.fillStyle = "white";
+        context.fillRect(x,y,1,1);
+    }
 }
+window.onload = codeAddress;
