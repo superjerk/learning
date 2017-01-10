36fe5f9bc040870448fbe81b695fd5091b4fd7a3
superjerk <tungsten33@gmail.com>
2017-01-10 14:57:17 -0600

    Update learning.js

--- learning.js
+++ learning.js
@@ -1,3 +1,6 @@
+function getRandom(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 function setCanvas() {
     var canvas = document.getElementById("starfield");
     canvas.style.width='100%';
@@ -6,11 +9,17 @@ function setCanvas() {
     canvas.height = canvas.offsetHeight;
     context = canvas.getContext("2d");
     stars = 300;
+    colorrange = [0,60,240];
     for (var i = 0; i < stars; i++) {
-        x = Math.random() * canvas.offsetWidth;
-        y = Math.random() * canvas.offsetHeight;
-        context.fillStyle = "white";
-        context.fillRect(x,y,1,1);
+        var x = Math.random() * canvas.offsetWidth;
+        y = Math.random() * canvas.offsetHeight,
+        radius = Math.random() * 1.2,
+        hue = colorrange[getRandom(0,colorrange.length - 1)],
+        sat = getRandom(50,100);
+        context.beginPath();
+        context.arc(x, y, radius, 0, 360);
+        context.fillStyle = "hsl(" + hue + ", " + sat + "%, 88%)";
+        context.fill();
     }
 }
 window.onload = setCanvas;
