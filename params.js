0b996a07f519ee8c092df4db06e820709d186e0f
superjerk <tungsten33@gmail.com>
2017-01-10 15:01:08 -0600

    Create params.js

---
+++ params.js
@@ -0,0 +1,5 @@
+Energy Gen	Energy Storage	Hull	Shield	Mass Weapons	Energy Weapon	Plasma Weapon
+Combustion	Batteries	Ferric	EM	Bulletstorm	Laser	Beam
+Fission	Hydrogen	Aluminum	Charged Parti	Railgun	Blaster	Blade
+Fusion	Capacitors	Ceramics	Phasing	Missile	Photon Torpedo	Blob
+Solar	Flywheel	Plastics	Deflecting	Rocket	Ion Cannon	Rope/Hose
