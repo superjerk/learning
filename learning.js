6b8d3d2fa18b91a06de468a3c960a02b70307f17
superjerk <tungsten33@gmail.com>
2017-01-10 14:53:48 -0600

    Update learning.js

--- learning.js
+++ learning.js
@@ -1,11 +1,11 @@
-function codeAddress() {
+function setCanvas() {
     var canvas = document.getElementById("starfield");
     canvas.style.width='100%';
     canvas.style.height='100%';
     canvas.width  = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
     context = canvas.getContext("2d");
-    stars = 200;
+    stars = 300;
     for (var i = 0; i < stars; i++) {
         x = Math.random() * canvas.offsetWidth;
         y = Math.random() * canvas.offsetHeight;
@@ -13,4 +13,4 @@ function codeAddress() {
         context.fillRect(x,y,1,1);
     }
 }
-window.onload = codeAddress;
+window.onload = setCanvas;
