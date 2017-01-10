dbe63144e23283dd240dcd0b76f78eb2f063d327
superjerk <tungsten33@gmail.com>
2017-01-10 15:06:43 -0600

    Update params.js

--- params.js
+++ params.js
@@ -1,4 +1,9 @@
-Energy Gen	Energy Storage	Hull	Shield	Mass Weapons	Energy Weapon	Plasma Weapon
+var EnergyGen = ['Combustion','Fission','Fusion','Solar'], 	
+    EnergyStor =['Batteries','Hydrogen','Capacitors','Flywheel'],
+    Hull = ['Ferric','Aluminum','Ceramics','Plastics'],
+    Shield = ['EM','Charged','Phasing','Deflecting'],
+    MassWep = ['Bulletstorm','Railgun','Missile','Rocket'],	
+    Energy Weapon	Plasma Weapon
 Combustion	Batteries	Ferric	EM	Bulletstorm	Laser	Beam
 Fission	Hydrogen	Aluminum	Charged Parti	Railgun	Blaster	Blade
 Fusion	Capacitors	Ceramics	Phasing	Missile	Photon Torpedo	Blob
