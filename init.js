8d4f5e03c3c3c1e65427e2a40c93d68d2ccf4f33
superjerk <tungsten33@gmail.com>
2017-01-11 09:51:07 -0600

    Update init.js

--- init.js
+++ init.js
@@ -5,4 +5,4 @@ var EnergyGen = ['Combustion','Fission','Fusion','Solar'],
     MassWep = ['Bulletstorm','Railgun','Missile','Rocket'],	
     EnergyWep = ['Laser','Blaster','Torpedo','Ion Cannon'],	
     PlasmaWep = ['Beam','Blade','Blob','Rope'];
-var shipparts = [EnergyGen,EnergyStor,Hull,Shield,MassWep,EnergyWep,PlasmaWep];
+var shipparts = {EnergyGen: EnergyGen,EnergyStor: EnergyStore,Hull: Hull,Shield: Shield,MassWep: MassWep,EnergyWep: EnergyWep,PlasmaWep: PlasmaWep};
