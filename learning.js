a7952ff5a13d464f09d9feb6b283a9c61cc01e3b
superjerk <tungsten33@gmail.com>
2017-01-11 13:15:53 -0600

    Update learning.js

--- learning.js
+++ learning.js
@@ -24,7 +24,7 @@ function initpage() {
     }
     //create buttons and dropdowns
     Object.keys(shipparts).forEach(function(key) {
-        document.body.insertAdjacentHTML('beforeend','<div id="' + key + '-jqdd" class="jq-dropdown jq-dropdown-tip"><ul class="jq-dropdown-menu"><li><a href="#1">' + shipparts[key][0] + '</a></li><li><a href="#2">' + shipparts[key][1] + '</a></li><li><a href="#3">' + shipparts[key][2] + '</a></li><li><a href="#4">' + shipparts[key][3] + '</a></li></ul></div>');            
+        document.body.insertAdjacentHTML('beforeend','<div id="' + key + '-jqdd" class="jq-dropdown jq-dropdown-tip"><ul class="jq-dropdown-menu"><li><a href="#" id="' + shipparts[key][0] +'">' + shipparts[key][0] + '</a></li><li><a href="#" id="' + shipparts[key][1] +'">' + shipparts[key][1] + '</a></li><li><a href="#" id="' + shipparts[key][2] +'">' + shipparts[key][2] + '</a></li><li><a href="#" id="' + shipparts[key][3] +'">' + shipparts[key][3] + '</a></li></ul></div>');            
         document.getElementById("shipsheet").insertAdjacentHTML('beforeend','<div id="' + key + '" class="dd" data-jq-dropdown="#' + key + '-jqdd">' + key + '</div>');
     });
 }
