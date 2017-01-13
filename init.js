b39cbf3e12dd6e738890e51ecdf463c215606dca
superjerk <tungsten33@gmail.com>
2017-01-13 14:02:17 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-var game = {globals: {start: 0, time: 0},settings: {fps: 10}};
+var game = {globals: {start: 0, time: 0},fps: 10};
 var EnergyGen = ['Combustion','Fission','Fusion','Solar'], 	
     EnergyStor =['Batteries','Hydrogen','Capacitors','Flywheel'],
     Hull = ['Ferric','Aluminum','Ceramics','Plastics'],
@@ -7,3 +7,6 @@ var EnergyGen = ['Combustion','Fission','Fusion','Solar'],
     EnergyWep = ['Laser','Blaster','Torpedo','Ion Cannon'],	
     PlasmaWep = ['Beam','Blade','Blob','Rope'];
 var shipparts = {EnergyGen: EnergyGen,EnergyStor: EnergyStor,Hull: Hull,Shield: Shield,MassWep: MassWep,EnergyWep: EnergyWep,PlasmaWep: PlasmaWep};
+game.initialize = function() {
+  this.context = document.getElementById("starfield").getContext("2d");
+};
